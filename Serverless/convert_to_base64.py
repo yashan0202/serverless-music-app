d1640642e1cb73d3d2d39e311bb85607e349488f
Yash <yashuan0202@gmail.com>
2025-02-06 02:13:30 +0530

    Update convert_to_base64.py

--- Serverless/convert_to_base64.py
+++ Serverless/convert_to_base64.py
@@ -1,7 +1,7 @@
 import base64
 
 # Open the MP3 file and encode it in Base64
-with open("BlindingLights.mp3", "rb") as file:
+with open(r"C:\Users\yashu\Desktop\Zeinab\React (CWH)\serverless-music-app\Serverless\BlindingLights.mp3", "rb") as file:
     base64_encoded = base64.b64encode(file.read()).decode('utf-8')
 
 print(base64_encoded)  # Copy this output for the API request
